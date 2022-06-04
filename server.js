31228c654ba17deac3a98803aed9ba5b653f2d3e
Ben Abdallah Oussama <benabdalla.oussama@gmail.com>
2022-06-04 13:59:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/sakai-ng'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/sakai-ng/index.html'));
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
